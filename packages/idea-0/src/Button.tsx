1f12460b8c578d85da96eebaab08429c27ba71de
ryuilhwan <ih.ryu.dev@gmail.com>
2024-02-09 22:44:32 +0900

    Value완성

--- packages/idea-0/src/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ComponentPropsWithRef } from "react";
-
-type ButtonProps = ComponentPropsWithRef<"button">;
-export function Button(props: ButtonProps) {
-  return <button {...props} />;
-}
---
+++ packages/idea-0/src/Value/ Value.tsx
@@ -0,0 +1,43 @@
+declare module "react" {
+  function forwardRef<T, P = object>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactNode | null,
+  ): (props: P & React.RefAttributes<T>) => React.ReactNode | null;
+}
+type HTMLElements = HTMLElementTagNameMap & HTMLElementDeprecatedTagNameMap;
+type IntrinsicHTMLElements = {
+  [K in keyof JSX.IntrinsicElements]: K extends keyof HTMLElements
+    ? JSX.IntrinsicElements[K]
+    : never;
+};
+import React from "react";
+import {
+  FC,
+  ReactNode,
+  forwardRef,
+  ForwardedRef,
+  ComponentPropsWithRef,
+} from "react";
+
+export type $VALUE = {
+  $value: string | number | boolean;
+  $valueLabel: ReactNode;
+};
+
+export interface VALUECallback {
+  Root:
+    | string
+    | FC<$VALUE & ComponentPropsWithRef<keyof IntrinsicHTMLElements>>;
+}
+export type VALUEProps = $VALUE & VALUECallback;
+
+export const VALUE = forwardRef(function VALUE<T extends keyof HTMLElements>(
+  props: VALUEProps,
+  ref: ForwardedRef<HTMLElements[T]>,
+) {
+  const { Root, ...restProps } = props;
+  return <Root ref={ref} {...restProps} />;
+});
+
+function test() {
+  return <VALUE<"div"> Root="div" $value="test" $valueLabel="test" />;
+}
