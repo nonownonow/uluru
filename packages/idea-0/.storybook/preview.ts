83ee3771b400dba166457e2403df13807200c11d
ryuilhwan <ih.ryu.dev@gmail.com>
2024-02-13 19:02:34 +0900

    fix

--- packages/idea-0/.storybook/preview.ts
+++ packages/idea-0/.storybook/preview.ts
@@ -1,3 +1,16 @@
-import preview from "../../../.storybook/preview";
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    html: {
+      transform: (code) => code.replace(/data-testid=".+?"/g, ""),
+    },
+  },
+};
 
 export default preview;
