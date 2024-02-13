595836a5826881d236cfab448bd108c9fbba6e65
ryuilhwan <ih.ryu.dev@gmail.com>
2024-02-14 06:58:14 +0900

    fix

--- packages/idea-0/.storybook/main.ts
+++ packages/idea-0/.storybook/main.ts
@@ -1,6 +1,19 @@
-import config from "../../../.storybook/main";
-config.stories = [
-  "../src/**/*.mdx",
-  "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-];
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@whitespace/storybook-addon-html",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
 export default config;
--- packages/idea-0/.storybook/preview.ts
+++ packages/idea-0/.storybook/preview.ts
@@ -1,1 +1,16 @@
-export default "../../../.storybook/preview";
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    html: {
+      transform: (code) => code.replace(/data-testid=".+?"/g, ""),
+    },
+  },
+};
+
+export default preview;
