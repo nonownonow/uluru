13adf053144315e8d154fd45396b85aee9e1dcdf
ryuilhwan <ih.ryu.dev@gmail.com>
2024-02-14 07:38:39 +0900

    Updates

---
+++ assets/preview-CLNaw-t9.js
@@ -0,0 +1,1 @@
+const a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},html:{transform:e=>e.replace(/data-testid=".+?"/g,"")}}};export{a as default};
