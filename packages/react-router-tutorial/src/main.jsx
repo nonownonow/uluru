86a7ce5af5db03246ee16d502b5619e7909da365
ryuilhwan <ih.ryu.dev@gmail.com>
2024-02-17 23:46:26 +0900

    에러 핸들링

--- packages/react-router-tutorial/src/main.jsx
+++ packages/react-router-tutorial/src/main.jsx
@@ -3,15 +3,17 @@ import * as ReactDOM from "react-dom";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/root";
 import "./index.css";
+import ErrorPage from "./routes/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
